aa30a65d452617b8e09bc70fbb2a0e6e4a08008f
Richard Horrocks <richardjhorrocks@gmail.com>
2018-07-05 09:39:40 +0100

    2 - transfer.

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,13 +1,44 @@
 pragma solidity ^0.4.2;
 
 contract DappToken {
-    // Constructor.
-    // Set the total number of tokens.
-    // Read the total number of tokens.
-
+    // REQUIRED
     uint256 public totalSupply;
+    mapping (address => uint256) public balanceOf;
 
-    constructor() public {
-        totalSupply = 1000000;
+    // OPTIONAL
+    string public name = "DappToken";
+    string public symbol = "DAPP";
+
+    // NOT ERC-20
+    string public standard = "Dapp Token v1.0";
+
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
+
+    constructor(uint256 _initialSupply) public {
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
+    }
+
+    // Transfer tokens.
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success) {
+
+        // Raise an exception if the 'from' account doesn't have enough.
+        require(balanceOf[msg.sender] >= _value);
+
+        // Transfer the balance.
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        // Must emit the Transfer event.
+        Transfer(msg.sender, _to, _value);
+
+        // Must return a boolean.
+        return true;
     }
 }
