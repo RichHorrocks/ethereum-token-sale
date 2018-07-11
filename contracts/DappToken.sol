9d0508913d1e158a539451af441fe1404bf58090
Richard Horrocks <richardjhorrocks@gmail.com>
2018-07-11 07:55:08 +0100

    Complete token contract

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -61,12 +61,32 @@ contract DappToken {
         // Approve event.
         emit Approval(msg.sender, _spender, _value);
 
+        // MUST return a boolean.
         return true;
     }
 
-//    function transferFrom() {
+    function transferFrom(address _from, address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        // Require the from account has the tokens.
+        require(balanceOf[_from] >= _value);
 
-//    }
+        // Require the allowance is big enough.
+        require(allowance[_from][msg.sender] >= _value);
 
+        // Change the balance.
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        // Update the allowance.
+        allowance[_from][msg.sender] -= _value;
+
+        // MUST emit the Transfer event.
+        emit Transfer(_from, _to, _value);
+
+        // Return a boolean.
+        return true;
+    }
 
 }
