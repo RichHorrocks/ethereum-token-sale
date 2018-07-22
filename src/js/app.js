d71b08aa77a1d6d75536a4434c54974824e1df01
Richard Horrocks <richardjhorrocks@gmail.com>
2018-07-22 14:23:29 +0100

    Wire up JS to contracts

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,56 @@
-console.log("App.js loaded...");
+App = {
+  web3Provider: null,
+  contracts: {},
+  account: '0x0',
+
+  init: function () {
+    console.log('App initialised...');
+  },
+
+  initWeb3: function () {
+    if (typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+      console.log('Using Metamask...');
+    } else {
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      web3 = new Web3(App.web3Provider);
+      console.log('Using Ganache...');
+    }
+  },
+
+  initContracts: function () {
+    $.getJSON('DappTokenSale.json', function (dappTokenSale) {
+      App.contracts.DappTokenSale = TruffleContract(dappTokenSale);
+      App.contracts.DappTokenSale.setProvider(App.web3Provider);
+      App.contracts.DappTokenSale.deployed().then(function (dappTokenSale) {
+        console.log('Dapp Token Sale Address:', dappTokenSale.address);
+      });
+    }).done(function () {
+      $.getJSON('DappToken.json', function (dappToken) {
+        App.contracts.DappToken = TruffleContract(dappToken);
+        App.contracts.DappToken.setProvider(App.web3Provider);
+        App.contracts.DappToken.deployed().then(function (dappToken) {
+          console.log('Dapp Token Address:', dappToken.address);
+        });
+      });
+    });
+  },
+
+  render: function () {
+    // Load account data.
+    web3.eth.getCoinbase(function (err, account) {
+      if (err === null) {
+        App.account = account;
+        $('#accountAddress').html('Your Account: ' + account);
+      }
+    });
+  },
+};
+
+window.onload = function () {
+  App.init();
+  App.initWeb3();
+  App.initContracts();
+  App.render();
+};
