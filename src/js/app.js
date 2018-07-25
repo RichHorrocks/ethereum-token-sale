d122f660fd0414d26b4cf412314fa619077a841f
Richard Horrocks <richardjhorrocks@gmail.com>
2018-07-25 22:06:51 +0100

    Trying to get the JS to work...

--- src/js/app.js
+++ src/js/app.js
@@ -2,21 +2,23 @@ App = {
   web3Provider: null,
   contracts: {},
   account: '0x0',
+  loading: false,
+  tokenPrice: 1000000000000000,
 
   init: function () {
     console.log('App initialised...');
   },
 
   initWeb3: function () {
-    if (typeof web3 !== 'undefined') {
-      App.web3Provider = web3.currentProvider;
-      web3 = new Web3(web3.currentProvider);
-      console.log('Using Metamask...');
-    } else {
+//    if (typeof web3 !== 'undefined') {
+//      App.web3Provider = web3.currentProvider;
+//      web3 = new Web3(web3.currentProvider);
+  //    console.log('Using Metamask...');
+  //  } else {
       App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
       web3 = new Web3(App.web3Provider);
       console.log('Using Ganache...');
-    }
+  //  }
   },
 
   initContracts: function () {
@@ -38,6 +40,18 @@ App = {
   },
 
   render: function () {
+    if (App.loading) {
+      return;
+    }
+
+    App.loading = true;
+
+    var loader = $('#loader');
+    var content = $('#content');
+
+    loader.show();
+    content.hide();
+
     // Load account data.
     web3.eth.getCoinbase(function (err, account) {
       if (err === null) {
@@ -45,6 +59,18 @@ App = {
         $('#accountAddress').html('Your Account: ' + account);
       }
     });
+
+    App.contracts.DappTokenSale.deployed().then(function (instance) {
+      dappTokenSaleInstance = instance;
+      return dappTokenSaleInstance.tokenPrice();
+    }).then(function (tokenPrice) {
+      App.tokenPrice = tokenPrice;
+      $('.token-price').html(web3.fromWei(App.tokenPrice, 'ether').toNumber());
+    });
+
+    App.loading = false;
+    loader.hide();
+    content.show();
   },
 };
 
